3831832414d37882b829bda29adbc5bb4102f97b
Chen-Oliver <ochen1@stuy.edu>
2016-10-29 17:40:53 -0400

    time

--- stats.c
+++ stats.c
@@ -3,14 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 
 int main(){
   struct stat *file = (struct stat*)malloc(sizeof(struct stat));
   stat("testfile",file);
   printf("size: %lld\n",file->st_size);
   printf("permissions: %o\n",file->st_mode);
-  printf("last accessed: %d\n",file->st_atimespec);
+  //printf("last accessed: %d\n",file->st_atime);
+  struct tm *accessed = localtime(&(file->st_atime));
+  printf("%d-%d-%d\n",accessed->tm_year,accessed->tm_mon,accessed->tm_mday);
   free(file);
   return 0;
 }
